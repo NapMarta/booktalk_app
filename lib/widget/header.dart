75181083aa17e5ab15453ac7e2fc86d24a535982
NapMarta <marta.napolillo@outlook.it>
2024-01-07 17:49:56 +0100

    Aggiunta messaggio di conferma Logout

--- lib/widget/header.dart
+++ lib/widget/header.dart
@@ -29,6 +29,7 @@ class Header extends StatefulWidget {
 }
 
 class _HeaderState extends State<Header> {
+  
   @override
   Widget build(BuildContext context) {
     var mediaQueryData = MediaQuery.of(context);
@@ -80,14 +81,79 @@ class _HeaderState extends State<Header> {
             padding: EdgeInsets.only(right: 0),
             child:
               widget.isProfilo
-              ? IconButton(
-                  onPressed: () { 
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => BookTalkApp(),
-                      ),
-                    );
-                  },
+              ? 
+                // ----- ALLERT CONFERMA LOGOUT -----
+                IconButton(
+                  onPressed: () => showDialog(
+                    context: context,
+                    builder: (_) => Dialog(
+                      child: Container(
+                        padding: EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(25.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Logout',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: size(mediaQueryData.size.width, mediaQueryData.size.height, 20),
+                              ),
+                            ),
+                            SizedBox(height: 10.0),
+                            Text(
+                              'Sei sicuro di voler fare il logout?',
+                              style: TextStyle(fontSize: size(mediaQueryData.size.width, mediaQueryData.size.height, 16)),
+                            ),
+                            SizedBox(height: 20.0),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(Color(0xFF0097b2)),
+                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20), // Adjust the value as needed
+                                      ),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    // Handle logout action
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                      builder: (context) => BookTalkApp(),
+                                      ),
+                                    ); 
+                                  },
+                                  child: Text('Logout', style: TextStyle(color: Colors.white)),
+                                ),
+                                SizedBox(width: 20.0),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop(); 
+                                  },
+                                  child: Text('Annulla', style: TextStyle(color: Color(0xFF0097b2))),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ) 
+                      ,
+                    ),
+                  ),
                   icon: Image.asset("assets/logout.png"),
                   iconSize: 0,
                 )
