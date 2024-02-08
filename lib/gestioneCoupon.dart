a3e8f43d4c51df55b40ce55aa99b635cffdc918c
simoLoc <simonaloc1@yahoo.it>
2024-02-08 17:00:55 +0100

    Aggiunta gestioneCoupon

---
+++ lib/gestioneCoupon.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:booktalk_app/aggiuntaLibroResponsive.dart';
+import 'package:booktalk_app/widget/header.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async' show Future, runZonedGuarded;
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:typed_data';
+
+Future<void> verificaCoupon() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Prendere i parametri isbn e coupon da quelli che ha inserito l'utente
+  String isbn = '8806134965';
+  String coupon = 'A45DF3';
+
+  final url = 'http://130.61.22.178:9000/verificaCoupon';
+
+  final response = await http.post(Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'isbn': isbn, 'coupon': coupon}));
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    if (data['result']) {
+      print('Il libro è supportato e il codice coupon è valido.');
+      //Aggiungere il libro al database e messaggio di conferma
+    } else {
+      print('Libro attualmente non supportato o codice coupon non valido.');
+      //Stampare messaggio di errore
+    }
+  } else {
+    throw Exception('Errore durante la richiesta HTTP');
+  }
+}
