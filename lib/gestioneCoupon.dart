e09b3a960c40da62d912912000796e24e081defc
luigina <l.costante1@studenti.unisa.it>
2024-02-09 20:38:21 +0100

    aggiunta autorizzazione nel DB per coupon validi

--- lib/gestioneCoupon.dart
+++ lib/gestioneCoupon.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:booktalk_app/aggiuntaLibroResponsive.dart';
+import 'package:booktalk_app/business_logic/gestioneAutorizzazioni.dart';
+import 'package:booktalk_app/business_logic/gestioneAutorizzazioniService.dart';
 import 'package:booktalk_app/widget/header.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -8,8 +10,11 @@ import 'dart:async' show Future, runZonedGuarded;
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:typed_data';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 Future<void> verificaCoupon(String isbn, String coupon) async {
   WidgetsFlutterBinding.ensureInitialized();
+  int idUtente = 0;
 
   //Prendere i parametri isbn e coupon da quelli che ha inserito l'utente
   //String isbn = '8806134965';
@@ -25,7 +30,18 @@ Future<void> verificaCoupon(String isbn, String coupon) async {
     final data = jsonDecode(response.body);
     if (data['result']) {
       print('Il libro è supportato e il codice coupon è valido.');
-      //Aggiungere il libro al database
+
+      SharedPreferences _preferences = await SharedPreferences.getInstance();
+      String utenteJson = _preferences.getString('utente') ?? '';
+      if (utenteJson.isNotEmpty) {
+        Map<String, dynamic> utenteMap = json.decode(utenteJson);
+        idUtente = int.parse(utenteMap['ID'].toString());
+        GestioneAutorizzazioniService service = GestioneAutorizzazioni();
+        service.addAutorizzazione(isbn, idUtente);
+      }
+      else{
+        print("ERRORE: utente non trovato");
+      }
       
       //messaggio di conferma
     } else {
