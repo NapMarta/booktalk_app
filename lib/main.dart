785bc61f5ea23e6d77034e824ef88dfd632abb9b
simoLoc <simonaloc1@yahoo.it>
2024-02-09 17:28:28 +0100

    modifica orientamento schermo

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:path/path.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:device_info/device_info.dart';
+import 'dart:ui';
 import 'utils.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+  var physicalWidth = window.physicalSize.width;
+  var screenWidth = physicalWidth / window.devicePixelRatio;
+
+  if (screenWidth >= 600) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.portraitDown
+    ]);
+  } else {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   // final view = WidgetsBinding.instance.platformDispatcher.views.first;
   /*
   // --- Settaggio Orientamento per smartphone e tablet
   if(view.physicalSize.width < 700 && view.physicalSize.height < 700){
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
+    
   }else{
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
@@ -31,15 +50,14 @@ Future<void> main() async {
     ]);
   }*/
 
-  //await initializeApp(context as BuildContext); // Chiamata alla funzione di inizializzazione  
-  
+  //await initializeApp(context as BuildContext); // Chiamata alla funzione di inizializzazione
+
   /*runApp(MaterialApp(
     home: BookTalkApp(),
   ));*/
   runApp(MaterialApp(
     home: await initializeApp(), // Chiamata alla funzione di inizializzazione
   ));
-  
 
   //extract_text();
 }
@@ -63,13 +81,13 @@ Future<Widget> initializeApp() async {
     else
       print("ERRORE");
 
-      if (utenteMap['ULTFUNZ1'] != null) 
-        monitoraggio.setFunz1(double.parse(utenteMap['ULTFUNZ1']));
-      if (utenteMap['ULTFUNZ2'] != null)
-        monitoraggio.setFunz2(double.parse(utenteMap['ULTFUNZ2']));
-      if (utenteMap['ULTFUNZ3'] != null)
-        monitoraggio.setFunz3(double.parse(utenteMap['ULTFUNZ3']));
-  
+    if (utenteMap['ULTFUNZ1'] != null)
+      monitoraggio.setFunz1(double.parse(utenteMap['ULTFUNZ1']));
+    if (utenteMap['ULTFUNZ2'] != null)
+      monitoraggio.setFunz2(double.parse(utenteMap['ULTFUNZ2']));
+    if (utenteMap['ULTFUNZ3'] != null)
+      monitoraggio.setFunz3(double.parse(utenteMap['ULTFUNZ3']));
+
     return HomepageResponsitive();
   } else {
     print('Nessun utente memorizzato');
@@ -77,10 +95,7 @@ Future<Widget> initializeApp() async {
   }
 }
 
-
-
 class BookTalkApp extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
     var mediaQueryData = MediaQuery.of(context);
@@ -91,71 +106,88 @@ class BookTalkApp extends StatelessWidget {
         // Restituisci 'false' per impedire la navigazione indietro.
         return false;
       },
-    child: MaterialApp(
-      home: Scaffold(
-      backgroundColor: Colors.white,
+      child: MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // ------ LOGO ------
+                Image.asset('assets/logo_noSfondo.png',
+                    height: logoSize(mediaQueryData.size.width,
+                        mediaQueryData.size.height, 0.2)),
+                SizedBox(height: isTabletOrizzontale(mediaQueryData) ? 10 : 40),
 
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-
-              // ------ LOGO ------
-              Image.asset('assets/logo_noSfondo.png', height: logoSize(mediaQueryData.size.width, mediaQueryData.size.height, 0.2)),
-              SizedBox(height: isTabletOrizzontale(mediaQueryData) ? 10 : 40), 
-
-              // ------ Pulsante "Registrati" ------
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => RegistrazioneResponsive(),
-                    ),
-                  );
-                },
-                
-                style: ButtonStyle(
-                  fixedSize: MaterialStateProperty.all(Size(buttonWidth(mediaQueryData.size.width, mediaQueryData.size.height), 55)),
-                  backgroundColor: MaterialStateProperty.all(Color(0xFF1B536E)),
-                  textStyle: MaterialStateProperty.all(TextStyle(fontSize: 15, fontWeight: FontWeight.bold,)),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20), // Adjust the value as needed
+                // ------ Pulsante "Registrati" ------
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => RegistrazioneResponsive(),
+                      ),
+                    );
+                  },
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(Size(
+                        buttonWidth(mediaQueryData.size.width,
+                            mediaQueryData.size.height),
+                        55)),
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xFF1B536E)),
+                    textStyle: MaterialStateProperty.all(TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    )),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            20), // Adjust the value as needed
+                      ),
                     ),
                   ),
+                  child:
+                      Text('Registrati', style: TextStyle(color: Colors.white)),
                 ),
-                child: Text('Registrati', style: TextStyle(color: Colors.white)),
-              ),
 
-              SizedBox(height: mediaQueryData.size.height * 0.02), // Spazio tra i pulsanti
+                SizedBox(
+                    height: mediaQueryData.size.height *
+                        0.02), // Spazio tra i pulsanti
 
-              // ------ Pulsante "Accedi" ------
-              ElevatedButton(
-                onPressed: () {
+                // ------ Pulsante "Accedi" ------
+                ElevatedButton(
+                  onPressed: () {
                     Navigator.of(context).push(
                       MaterialPageRoute(
                         builder: (context) => LoginResponsive(),
                       ),
-                  ); 
-                },
-                style: ButtonStyle(
-                  fixedSize: MaterialStateProperty.all(Size(buttonWidth(mediaQueryData.size.width, mediaQueryData.size.height), 55)),
-                  backgroundColor: MaterialStateProperty.all(Color(0xFF0097B2)),
-                  textStyle: MaterialStateProperty.all(TextStyle(fontSize: 15, fontWeight: FontWeight.bold,)),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20), // Adjust the value as needed
+                    );
+                  },
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(Size(
+                        buttonWidth(mediaQueryData.size.width,
+                            mediaQueryData.size.height),
+                        55)),
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xFF0097B2)),
+                    textStyle: MaterialStateProperty.all(TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    )),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            20), // Adjust the value as needed
+                      ),
                     ),
                   ),
+                  child: Text('Login', style: TextStyle(color: Colors.white)),
                 ),
-                child: Text('Login', style: TextStyle(color: Colors.white)),
-              ),
-
-            ],
+              ],
+            ),
           ),
         ),
       ),
-    ),
     );
   }
 }
